3f370816f3f01a29d58dc07f12b9e459104b61d6
zjp18538666496 <1975922551@qq.com>
2024-07-12 09:42:48 +0800

    zjp

--- web/src/views/role-manager/components/EditRole.vue
+++ web/src/views/role-manager/components/EditRole.vue
@@ -9,12 +9,50 @@
         <el-form-item prop="description" label="角色描述">
             <el-input v-model="props.roleInfo.description" />
         </el-form-item>
+        <el-form-item prop="description" label="菜单权限">
+            <el-tree-select
+                v-model="terrNode"
+                :data="menuList"
+                :render-after-expand="false"
+                :props="defaultProps"
+                multiple
+                collapse-tags
+                show-checkbox
+                :check-strictly="false"
+                @current-change="currentChange"
+                ref="tree"
+                @check-change="getCheckedNodes"
+            ></el-tree-select>
+        </el-form-item>
     </el-form>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { getMenuLists } from '@/api/menu.js'
+
+const terrNode = ref()
+const tree = ref()
+const menuList = ref([])
 const ruleFormRef = ref()
+const defaultProps = {
+    children: 'children', // 指定子树为节点对象的某个属性值
+    label: 'menu_name', // 指定节点标签为节点对象的某个属性值
+    value: 'id', // 指定节点值为节点对象的某个属性值
+    disabled: 'xxx',
+}
+getMenuLists({}).then((res) => {
+    if (res?.code === 200) {
+        menuList.value = [
+            {
+                id: 0,
+                menu_name: '根菜单',
+                disabled: false,
+                children: res.data,
+            },
+        ]
+    }
+})
 const props = defineProps({
     roleInfo: {
         type: Object,
@@ -25,6 +63,19 @@ const props = defineProps({
         required: true,
     },
 })
+const getCheckedNodes = () => {
+    // 获取所有选中节点的数据
+    terrNode.value = []
+    const checkedNodes = tree.value.getCheckedNodes()
+    checkedNodes.forEach(function (item) {
+        terrNode.value.push(item.id)
+    })
+    console.log('选中的节点数据:', checkedNodes)
+}
+const currentChange = (node) => {
+    terrNode.value = node.value
+    console.log(terrNode)
+}
 defineExpose({
     ruleFormRef,
 })
