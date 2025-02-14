8f981f6542522b44bbbb4625436fc2132a35ee59
zjp18538666496 <1975922551@qq.com>
2025-02-14 11:50:22 +0800

    更新sql

--- sql/pigx_admin.sql
+++ sql/pigx_admin.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Dump SQL
 
- Source Server         : mysql
+ Source Server         : localhost
  Source Server Type    : MySQL
  Source Server Version : 50744 (5.7.44)
  Source Host           : localhost:3306
@@ -11,41 +11,11 @@
  Target Server Version : 50744 (5.7.44)
  File Encoding         : 65001
 
- Date: 15/07/2024 09:06:56
+ Date: 14/02/2025 11:28:36
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for role_menu
--- ----------------------------
-DROP TABLE IF EXISTS `role_menu`;
-CREATE TABLE `role_menu`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `menu_id` int(11) NOT NULL COMMENT '菜单id',
-  `role_id` int(11) NOT NULL COMMENT '角色id',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_role_role_sys_menu_1`(`role_id`) USING BTREE,
-  INDEX `fk_role_role_sys_menu_2`(`menu_id`) USING BTREE,
-  CONSTRAINT `fk_role_role_sys_menu_1` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_role_role_sys_menu_2` FOREIGN KEY (`menu_id`) REFERENCES `sys_menu` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 124 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for role_permission
--- ----------------------------
-DROP TABLE IF EXISTS `role_permission`;
-CREATE TABLE `role_permission`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `role_id` int(11) NOT NULL COMMENT '用户id',
-  `permission_id` int(11) NOT NULL COMMENT '权限id',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_role_permission_sys_permission_1`(`permission_id`) USING BTREE,
-  INDEX `fk_role_permission_sys_role_1`(`role_id`) USING BTREE,
-  CONSTRAINT `fk_role_permission_sys_permission_1` FOREIGN KEY (`permission_id`) REFERENCES `sys_permission` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_role_permission_sys_role_1` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户权限表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for sys_menu
@@ -55,23 +25,14 @@ CREATE TABLE `sys_menu`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `parent_id` int(40) NOT NULL COMMENT '父级id',
   `menu_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '菜单名称',
-  `route` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单路由地址\r\n',
+  `route` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单路由地址',
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单状态',
-  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '	type  菜单类型 (0: 目录, 1: 菜单, 2: 按钮)',
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单类型 (0: 目录, 1: 菜单, 2: 按钮)',
   `level` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'level',
+  `component_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '路由组件地址',
+  `component_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '路由组件名称',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 100105 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for sys_permission
--- ----------------------------
-DROP TABLE IF EXISTS `sys_permission`;
-CREATE TABLE `sys_permission`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限名称',
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限描述',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '权限表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 20105 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_role
@@ -83,7 +44,18 @@ CREATE TABLE `sys_role`  (
   `role_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名称',
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色描述',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 38 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 104 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限名称',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限描述',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '权限表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -99,8 +71,35 @@ CREATE TABLE `sys_user`  (
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
   `last_login_time` datetime NULL DEFAULT NULL COMMENT '最后登录时间',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户头像地址',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for role_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `role_menu`;
+CREATE TABLE `role_menu`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `menu_id` int(11) NOT NULL COMMENT '菜单id',
+  `role_id` int(11) NOT NULL COMMENT '角色id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_role_role_sys_menu_1`(`role_id`) USING BTREE,
+  INDEX `fk_role_role_sys_menu_2`(`menu_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 43 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `role_id` int(11) NOT NULL COMMENT '用户id',
+  `permission_id` int(11) NOT NULL COMMENT '权限id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_role_permission_sys_permission_1`(`permission_id`) USING BTREE,
+  INDEX `fk_role_permission_sys_role_1`(`role_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户权限表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user_role
@@ -112,9 +111,53 @@ CREATE TABLE `user_role`  (
   `role_id` int(40) NOT NULL COMMENT '角色id',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `fk_user_role_sys_role_1`(`role_id`) USING BTREE,
-  INDEX `fk_user_role_sys_user_1`(`user_id`) USING BTREE,
-  CONSTRAINT `fk_user_role_sys_role_1` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_user_role_sys_user_1` FOREIGN KEY (`user_id`) REFERENCES `sys_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 34 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户角色表' ROW_FORMAT = DYNAMIC;
+  INDEX `fk_user_role_sys_user_1`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户角色表' ROW_FORMAT = Dynamic;
+
+-- Now, set the foreign key constraints
+ALTER TABLE `role_menu`
+  ADD CONSTRAINT `fk_role_role_sys_menu_1` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_role_role_sys_menu_2` FOREIGN KEY (`menu_id`) REFERENCES `sys_menu` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `role_permission`
+  ADD CONSTRAINT `fk_role_permission_sys_permission_1` FOREIGN KEY (`permission_id`) REFERENCES `sys_permission` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_role_permission_sys_role_1` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `user_role`
+  ADD CONSTRAINT `fk_user_role_sys_role_1` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user_role_sys_user_1` FOREIGN KEY (`user_id`) REFERENCES `sys_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- ----------------------------
+-- Insert records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES (2, 0, '系统管理', '/system', '1', '0', '1', NULL, NULL);
+INSERT INTO `sys_menu` VALUES (201, 2, '权限管理', '/permissions-manager', '1', '0', '2', NULL, NULL);
+INSERT INTO `sys_menu` VALUES (20101, 201, '角色管理', '/role-manager', '1', '1', '3', '@/views/role-manager/Index.vue', 'role-manager');
+INSERT INTO `sys_menu` VALUES (20102, 201, '用户管理', '/user-manager', '1', '1', '3', '@/views/user-manager/Index.vue', 'user-manager');
+INSERT INTO `sys_menu` VALUES (20103, 201, '菜单管理', '/menu-manager', '1', '1', '3', '@/views/menu-manager/Index.vue', 'menu-manager');
+
+-- ----------------------------
+-- Insert records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES (102, 'root', '管理员', '管理员');
+
+-- ----------------------------
+-- Insert records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES (2, '超级管理员', '$2a$10$9Ifg5wOb6hu94S0TcUdl/u94Uv5SMXucJPFTeQ2WeUhlWdx.z34l2', 'root', '185****6496', '1975922551@qq.com', '2025-02-13 18:12:42', '2025-02-14 11:28:14', '2025-02-13 18:12:54', NULL);
+
+-- ----------------------------
+-- Insert records of role_menu
+-- ----------------------------
+INSERT INTO `role_menu` VALUES (37, 2, 102);
+INSERT INTO `role_menu` VALUES (38, 201, 102);
+INSERT INTO `role_menu` VALUES (39, 20101, 102);
+INSERT INTO `role_menu` VALUES (40, 20102, 102);
+INSERT INTO `role_menu` VALUES (41, 20103, 102);
+
+-- ----------------------------
+-- Insert records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (5, 2, 102);
 
 SET FOREIGN_KEY_CHECKS = 1;
